45d7dd4c6c9a5413afa5407cdd1a60ead61242dc
Denis Yuliyanov <deniskaradaliev@gmail.com>
2023-03-29 22:41:50 +0200

    me jode la extraccion de los datos

--- src/Componentes/NuevasPaginas/Historico.js
+++ src/Componentes/NuevasPaginas/Historico.js
@@ -5,44 +5,19 @@ import Login from "./Login";
 function Historico(props) {
 
   const [productos, setProductos] = useState([]);
-  const [arrayHistorico,setArrayHistorico]=useState({});
+  const [arrayHistorico, setArrayHistorico] = useState([]);
 
   useEffect(() => {
     // console.log('SE MONTA PRODUCTOS');
     // PARA METER CUALQUIER LOGICA, ENVOLVER EN FUNCION
     // console.log(props.token);
     if (props.login != false && props.id != '') {
-      // console.log(props.id);
-      // console.log(props.login);
+      console.log(props.id);
+      console.log(props.login);
       axios.get('https://dsm-2023-default-rtdb.europe-west1.firebasedatabase.app//pedidos.json?orderBy="$key"&equalTo="' + props.id + '"')
         .then((response) => {
           // console.log(response.data[props.id]);
-
-
-          const pedido=response.data[props.id];
-          
-          pedido.array.forEach(element => {
-            
-          });
-          // for (let key in response.data[props.id]) {
-            
-          //   pedido.push({
-          //     id:key,
-          //     nombre: response.data[props.id][key].nombre,
-          //     apellido: response.data[props.id][key].apellido,
-          //     direccion:response.data[props.id][key].direccion,
-          //     precio: response.data[props.id][key].precio
-          //   })
-          //   console.log(pedido);
-            // setArrayHisotrico(pedido);
-            // console.log(arrayHistorico);
-
-          // }
-
-
-          
-
-
+          setArrayHistorico(response.data[props.id]);
         }).catch((error) => {
           alert('se ha producido un error');
           // esto es por si nos salra algun error en la base de datos
@@ -63,9 +38,21 @@ function Historico(props) {
 
   return (
 
+    <>
+      <p>Tu historico es...</p>
 
-    <p>Tu historico es...</p>
+      {/* {arrayHistorico.map((elemento) => {
 
+        return (
+          <div key={elemento.nombre}>
+            <h2>{elemento.apellidos}</h2>
+            <h2>{elemento.direccion}</h2>
+          </div>
+
+        )
+
+      })} */}
+    </>
   )
 }
 export default Historico;
